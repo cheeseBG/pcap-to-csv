f6e024f651ed3b61df12bc260a7e6b7f03ebbf22
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2023-08-03 11:31:39 +0900

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -5,15 +5,15 @@ os.makedirs('data/csv/', exist_ok=True)
 
 save_dir = 'data/csv/'
 
-# for pcap_fname in os.listdir('data/pcap'):
-#     df = pcap_to_df(os.path.join('data/pcap', pcap_fname))
-#     csv_fname = pcap_fname.split('.')[0] + '.csv'
-#     df.to_csv(os.path.join(save_dir, csv_fname), index=False)
-#     print(f'Save {csv_fname}')
-
-# ------ mat ------------
-for mat_fname in os.listdir('data/mat/test_A2/walk/'):
-    df = mat_to_df(os.path.join('data/mat/test_A2/walk/', mat_fname))
-    csv_fname = mat_fname.split('.')[0] + '.csv'
+for pcap_fname in os.listdir('data/pcap'):
+    df = pcap_to_df(os.path.join('data/pcap', pcap_fname))
+    csv_fname = pcap_fname.split('.')[0] + '.csv'
     df.to_csv(os.path.join(save_dir, csv_fname), index=False)
     print(f'Save {csv_fname}')
+
+# ------ mat ------------
+# for mat_fname in os.listdir('data/mat/test_A2/walk/'):
+#     df = mat_to_df(os.path.join('data/mat/test_A2/walk/', mat_fname))
+#     csv_fname = mat_fname.split('.')[0] + '.csv'
+#     df.to_csv(os.path.join(save_dir, csv_fname), index=False)
+#     print(f'Save {csv_fname}')
